24ca7eb2e1020f9d3860d7e2318b443099207eee
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-02-06 13:53:48 +0300

    Completion of the design and metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar
+        <Navbar />
         <main className="max-auto max-w-3xl px-3 py-10">{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return <header className="sticky top-0 bg-background">
         <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap3 py-3 px-4">
-            
+          <nav className="space-x-4 font-medium">
+            <Link href="/">Home</Link>
+            <Link href="/about">About me</Link>
+            <Link href="/social">Social media</Link>
+            </nav>  
         </div>
 
     </header>
