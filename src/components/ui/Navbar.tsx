aff86e8a640645638d8dbb5f31fce42465ed0e2d
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-02-06 13:28:32 +0300

    Design & metadata

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return <header className="sticky top-0 bg-background">
+        <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap3 py-3 px-4">
+            
+        </div>
+
+    </header>
+}
