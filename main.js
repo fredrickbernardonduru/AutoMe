7b95af5f8298895a8f22a658ba819e6d47abdec6
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2024-07-26 22:29:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
-
 /* Menu show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
