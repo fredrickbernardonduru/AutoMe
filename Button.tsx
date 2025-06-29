fbb9cb4a74af9c8308c235c977bcfed6a03b6956
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2025-06-29 22:01:08 +0300

    Update Button.tsx

--- Button.tsx
+++ Button.tsx
@@ -1,4 +1,4 @@
-trxdimport { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps<T extends React.ElementType> {
