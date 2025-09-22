dfc517673fa4fba8652c9043f85154366d86c4d0
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2025-09-22 19:53:16 +0300

    Update Button.tsx

--- Button.tsx
+++ Button.tsx
@@ -1,4 +1,4 @@
-
+ddggfhh
 import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -20,6 +20,7 @@ export default function Button<T extends React.ElementType = "button">({
     />
   );
 }
+
 
 
 
