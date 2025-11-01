d020d8833348762317c5d02b46334d6429174e61
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2025-11-01 23:32:52 +0300

    Fix formatting issues in Loan Prediction.py

--- Loan Prediction.py
+++ Loan Prediction.py
@@ -1,3 +1,4 @@
+uehshdjdjsbabsbcbjfjg
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -25,6 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, rand
 clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
  max_depth=3, min_samples_leaf=5)
 clf_entropy.fit(X_train, y_train)
+
 
 
 
