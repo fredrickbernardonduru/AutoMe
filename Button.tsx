9f62919f3c4dfefc220e71baf19f9cb8f4e278f6
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2025-11-25 23:34:19 +0300

    Refactor Button component with TypeScript support

--- Button.tsx
+++ Button.tsx
@@ -1,4 +1,4 @@
-hhhjjdjxjdj
+hhhjjdj
 import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -20,6 +20,7 @@ export default function Button<T extends React.ElementType = "button">({
     />
   );
 }
+
 
 
 
