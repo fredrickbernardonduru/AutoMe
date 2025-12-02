cd50a3bb1393de00f8684ab63e9d890b506dedfa
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2025-12-02 22:31:33 +0300

    Update LoadingButton.tsx

--- LoadingButton.tsx
+++ LoadingButton.tsx
@@ -1,4 +1,4 @@
-fgj
+
 import { ComponentPropsWithoutRef } from "react";
 import Button from "./Button";
 import { LoadingIndicator } from "stream-chat-react";
@@ -17,6 +17,7 @@ return (
     </Button>
 )
 }
+
 
 
 
